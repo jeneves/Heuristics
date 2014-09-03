ef2fcd6655b5f1e0c7578cd5a8d98f5313ce8957
Michael Jalkio <mjalkio@gmail.com>
2014-09-02 21:24:47 -0400

    Fixed style, add start of greedy deterministic

--- simpleOptimization.py
+++ simpleOptimization.py
@@ -3,9 +3,11 @@ import math
 import random
 import numpy
 
+
 # Calculates and returns the cost function
 def cost(s):
-    return (400 - (s-21) ** 2)  * math.sin(s * math.pi/6)
+    return (400 - (s - 21) ** 2) * math.sin(s * math.pi / 6)
+
 
 # Uses a neighborhood of max(scurrent -25,0) <= s <= min(scurrent +25, 500)
 #
@@ -19,8 +21,8 @@ def neighbor(s):
         s_new = random.randint(min_val, max_val)
     return s_new
 
-# Random Walk
 
+# Random Walk
 
 
 # Random Sampling
@@ -31,13 +33,14 @@ def random_sample_neighbor(s):
         s_new = random.randint(0, 500)
     return s_new
 
-def random_sample(s_initial, max_iter):
-    s_current=s_initial 
-    s_best=s_current
-    cost_current = cost(s_initial)
-    cost_best=cost_current
 
-    solution = numpy.zeros([max_iter + 1, 5]);
+def random_sample(s_initial, max_iter):
+    s_current = s_initial
+    s_best = s_current
+    cost_current = cost(s_initial)
+    cost_best = cost_current
+
+    solution = numpy.zeros([max_iter + 1, 5])
     solution[0] = [0, s_current, s_best, cost_current, cost_best]
 
     for i in range(max_iter):
@@ -46,18 +49,34 @@ def random_sample(s_initial, max_iter):
         if cost_current < cost_best:
             cost_best = cost_current
             s_best = s_current
-        solution[i+1] = [i+1, s_current, s_best, cost_current, cost_best]
+        solution[i + 1] = [i + 1, s_current, s_best, cost_current, cost_best]
 
     return solution
+
 
 # Deterministic neighborhood
 
 
-
 # Greedy Deterministic
+def gd_neighborhood(s):
+    min_val = max(s - 10, 0)
+    max_val = min(s + 10, 500)
+    return [neighbor for neighbor in range(min_val, max_val) if neighbor != s]
 
 
+def greedy_deterministic(s_initial, max_iter):
+    cost_best = cost(s_initial)
+    s_best = s_initial
+    i = 1
+    s_best_changed = True
+    while i <= max_iter and s_best_changed:
+        s_best_changed = False
+        neighborhood = gd_neighborhood(s_best)
+        for s_current in neighborhood:
+            cost_current = cost(s_current)
+            if cost_current < cost_best:
+                cost_best = cost_current
+                s_best = s_current
+                s_best_changed = True
 
 # Greedy Stochastic
-
-
