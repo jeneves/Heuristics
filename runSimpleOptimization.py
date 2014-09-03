f40a9b4134ffe28186aaba419ff8360e2d830b9b
Danny <dhs252@cornell.edu>
2014-09-02 21:53:07 -0400

    Basic file to run each of the optimizations 30 times.

---
+++ runSimpleOptimization.py
@@ -0,0 +1,17 @@
+
+import simpleOptimization as simple_opt
+import random
+
+max_iter = 200
+rw_solutions = ()
+rs_solutions = ()
+gd_solutions = ()
+gs_solutions = ()
+
+for i in range(30):
+
+    s_initial = random.randint(0, 500)
+    rw_solutions.append(simple_opt.random_walk(s_initial, max_iter))
+    rs_solutions.append(simple_opt.random_sample(s_initial, max_iter))
+    gd_solutions.append(simple_opt.greedy_deterministic(s_initial, max_iter))
+    gs_solutions.append(simple_opt.greedy_stochastic(s_initial, max_iter))
