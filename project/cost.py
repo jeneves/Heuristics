9c84deb95ec60f2e80586b2865bc7de4516bba4e
Danny <dhs252@cornell.edu>
2014-10-31 18:48:18 -0400

    Changed cost to take a binary matrix instead of a string.

--- project/cost.py
+++ project/cost.py
@@ -1,15 +1,15 @@
 
 
-# s is a string of length m x n, represents a matrix with m rows (one for each
-# cell) and n columns (one for each channel) concatenated together. Each value
-# in the string should be a binary 0 or 1.
+# s is a  a matrix with m rows (one for each cell) and n columns
+# (one for each channel) concatenated together. Each value in the
+# matrix should be a binary 0 or 1.
 # interference_matrix is an (m x m) matrix of interference values
 #
 # for this particular project, m = 7, n = 50.
 def cost(s, interference_matrix):
 
     num_cells = len(interference_matrix)
-    num_channels = len(s) / num_cells
+    num_channels = len(s[0])
 
     total_interference = 0
     for cell in range(0, num_cells):
@@ -23,8 +23,8 @@ def cost(s, interference_matrix):
                 for channel in range(0, num_channels):
 
                     # See if there's something to check
-                    value = get_value(s, cell, channel, num_channels)
-                    if value == '1':
+                    value = get_value(s, cell, channel)
+                    if value == 1:
 
                         min_check = 0
                         max_check = min(channel + distance + 1, num_channels)
@@ -38,15 +38,14 @@ def cost(s, interference_matrix):
 
                         # Actually perform the check
                         for check_channel in range(min_check, max_check):
-                            if get_value(s, check_cell, check_channel,
-                                         num_channels) == '1':
+                            if get_value(s, check_cell, check_channel) == 1:
                                 total_interference += 1
 
     return total_interference
 
 
-def get_value(s, cell, channel, num_channels):
-    return s[num_channels * cell + channel]
+def get_value(s, cell, channel):
+    return s[cell][channel]
 
 
 interference_matrix = [[2, 1, 0, 0, 0, 0, 0],
@@ -61,7 +60,3 @@ interference_matrix = [[2, 1, 0, 0, 0, 0, 0],
 # im = [[5, 2, 0, 0, 0, 2, 2], [2, 5, 2, 0, 0, 0, 2], [0, 2, 5, 2, 0, 0, 2],
 #       [0, 0, 2, 5, 2, 0, 2], [0, 0, 0, 2, 5, 2, 2], [2, 0, 0, 0, 2, 5, 2],
 #       [2, 2, 2, 2, 2, 2, 5]]
-# cost_12_s = '0010000000000000000100010000101000000000000000100000001000100000
-#              000010000000011000100001001'
-# cost_2_s = '00100000000000000001000010000000000000000001000010000000000000000
-#             100001000010000000000000000001000010010000100000100001'
